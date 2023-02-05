68d7a8b48cbd3d2eb8697ea86540d2093cbd6653
Shroominic <db2002dominic@gmail.com>
2023-02-05 15:49:39 +0100

    format with black

--- discordpy-bot/data_collection/bot.py
+++ discordpy-bot/data_collection/bot.py
@@ -7,26 +7,25 @@ import os
 
 
 class DataCollectionBot(commands.AutoShardedBot):
-    '''
+    """
     A bot for collecting data for Open Assistant.
-    '''
+    """
 
     def __init__(self, *args, **kwargs):
-        print('Initializing DataCollectionBot...')
-
+        print("Initializing DataCollectionBot...")
 
         super().__init__(*args, **kwargs)
 
     async def setup_hook(self):
-        self.remove_command('help')
+        self.remove_command("help")
 
-        for filename in os.listdir(os.path.dirname(__file__)+'/cogs/commands'):
-            if filename.endswith('.py'):
-                await self.load_extension(f'cogs.commands.{filename[:-3]}')
+        for filename in os.listdir(os.path.dirname(__file__) + "/cogs/commands"):
+            if filename.endswith(".py"):
+                await self.load_extension(f"cogs.commands.{filename[:-3]}")
 
-        for filename in os.listdir(os.path.dirname(__file__)+'/cogs/events'):
-            if filename.endswith('.py'):
-                await self.load_extension(f'cogs.events.{filename[:-3]}')
+        for filename in os.listdir(os.path.dirname(__file__) + "/cogs/events"):
+            if filename.endswith(".py"):
+                await self.load_extension(f"cogs.events.{filename[:-3]}")
 
 
 async def main():
@@ -35,14 +34,14 @@ async def main():
     intents = discord.Intents.all()
     intents.presences = False
 
-    bot = DataCollectionBot(        
-        command_prefix=os.getenv('PREFIX'),
+    bot = DataCollectionBot(
+        command_prefix=os.getenv("PREFIX"),
         intents=intents,
-        debug_guild=os.getenv('DECLARE_GLOBAL_COMMANDS'),
-        owner_id=os.getenv('OWNER_ID'),
+        debug_guild=os.getenv("DECLARE_GLOBAL_COMMANDS"),
+        owner_id=os.getenv("OWNER_ID"),
     )
 
-    await bot.start(os.getenv('BOT_TOKEN'))
+    await bot.start(os.getenv("BOT_TOKEN"))
 
 
 asyncio.run(main())
